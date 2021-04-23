27ea7a4d47a8502b4771cb420d01a525a4caf72e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-04-23 09:31:58 -0300

    create test for Welcomer module

---
+++ test/inmana/welcomer_test.exs
@@ -0,0 +1,34 @@
+defmodule Inmana.WelcomerTest do
+  use ExUnit.Case
+
+  alias Inmana.Welcomer
+
+  describe "welcome/1" do
+    test "when the user is special, returns a special message" do
+      params = %{"name" => "banana", "age" => "42"}
+      expected_result = {:ok, "You are very especial banana"}
+
+      result = Welcomer.welcome(params)
+
+      assert result == expected_result
+    end
+
+    test "when the user is not special, returns a message" do
+      params = %{"name" => "MaTeUs \n", "age" => "18"}
+      expected_result = {:ok, "Welcome mateus"}
+
+      result = Welcomer.welcome(params)
+
+      assert result == expected_result
+    end
+
+    test "when the user is under age, returns a error" do
+      params = %{"name" => "Mateus", "age" => "17"}
+      expected_result = {:error, "You shall not pass mateus"}
+
+      result = Welcomer.welcome(params)
+
+      assert result == expected_result
+    end
+  end
+end
